a2ad1150657fa412d2d6918e36bcb75ed4356d69
Igor <igor06brw@gmail.com>
2020-11-05 19:40:55 +0100

    setup bulma

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import '../styles/styles.scss'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1 className="title">Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
