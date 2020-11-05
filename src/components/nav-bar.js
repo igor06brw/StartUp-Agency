06a1ccb30d762c56cce87c9eb58cb14f8704ffe7
Igor <igor06brw@gmail.com>
2020-11-05 19:52:44 +0100

    add first classes of bulma in nav-bar.js

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -3,21 +3,21 @@ import { Link } from "gatsby"
 
 const NavBar = () => (
     <nav className="nav">
-        <ul className="nav__list">
+        <ul className="nav__list columns">
             <li className="nav__el">
-                <Link to="/" className="nav__link">FAQ</Link>
+                <Link to="/" className="nav__link column">FAQ</Link>
             </li>
             <li className="nav__el">
-                <Link to="/" className="nav__link">Pricing</Link>
+                <Link to="/" className="nav__link column">Pricing</Link>
             </li>
             <li className="nav__el">
-                <Link to="/" className="nav__link">Team</Link>
+                <Link to="/" className="nav__link column">Team</Link>
             </li>
             <li className="nav__el">
-                <Link to="/" className="nav__link">Contact</Link>
+                <Link to="/" className="nav__link column">Contact</Link>
             </li>
             <li className="nav__el">
-                <Link to="/" className="nav__link">Sign in</Link>
+                <Link to="/" className="nav__link column">Sign in</Link>
             </li>
         </ul>
     </nav>       
