aa89d7c38fe5514c3d63e399675bc2b47f83caa2
Igor <igor06brw@gmail.com>
2020-11-07 20:51:21 +0100

    using createPage to make pages from name file name md

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -5,16 +5,16 @@ const NavBar = () => (
     <nav className="nav level-right">
         <ul className="nav__list columns">
             <li className="nav__el">
-                <Link to="/" className="nav__link column is-size-6 has-text-white">FAQ</Link>
+                <Link to="/faq/" className="nav__link column is-size-6 has-text-white">FAQ</Link>
             </li>
             <li className="nav__el">
-                <Link to="/" className="nav__link column is-size-6 has-text-white">Pricing</Link>
+                <Link to="/pricing/" className="nav__link column is-size-6 has-text-white">Pricing</Link>
             </li>
             <li className="nav__el">
-                <Link to="/" className="nav__link column is-size-6 has-text-white">Team</Link>
+                <Link to="/team/" className="nav__link column is-size-6 has-text-white">Team</Link>
             </li>
             <li className="nav__el">
-                <Link to="/" className="nav__link column is-size-6 has-text-white">Contact</Link>
+                <Link to="/contact/" className="nav__link column is-size-6 has-text-white">Contact</Link>
             </li>
             <li className="nav__el">
                 <Link to="/" className="nav__link column is-size-6 has-text-white">Sign in</Link>
