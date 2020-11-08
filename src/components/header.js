7532a99eac51372ecfb96a0ce857e60b3dc6a52e
Igor <igor06brw@gmail.com>
2020-11-08 18:37:20 +0100

    justify title & subtitle of headeR

--- src/components/header.js
+++ src/components/header.js
@@ -6,16 +6,16 @@ const Header = ({ data }) => {
   const { headline, description, buttonOne, buttonTwo } = data
 
   return(
-    <header className="header columns pt-10">
+    <header className="header columns pt-7">
       <div className="header__body container is-fullwidth">
-        <div className="columns">
+        <div className="header__navigation columns">
           <Logo />
           <NavBar />
         </div>
-        <div className="hero pt-6">
-          <div className="hero-body has-text-centered">
-            <h1 className="title is-1 is-spaced has-text-white has-text-weight-normal">{ headline }</h1>
-            <p className="subtitle is-4 has-text-weight-light has-text-white">{ description }</p>
+        <div className="header__hero hero pt-6">
+          <div className="header__body hero-body has-text-centered is-centered">
+            <h1 className="header__title title is-1 is-spaced has-text-white has-text-weight-normal">{ headline }</h1>
+            <p className="header__subtitle subtitle has-text-weight-light has-text-white pt-8 ">{ description }</p>
           </div>
         </div>
       </div>
